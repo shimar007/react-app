421ec592d3b42118e2d90fdc81f04b6bdbd7c3eb
Shiva <shimar007@gmail.com>
2024-09-21 00:42:52 +1000

    adding basic tabbed screens

---
+++ app/(tabs)/contact.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Contact() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Contact Screen.</Text>
+    </View>
+  );
+}
